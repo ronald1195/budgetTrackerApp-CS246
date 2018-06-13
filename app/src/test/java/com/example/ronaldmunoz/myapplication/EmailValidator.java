6cd36d5e0d9937f13c1f9e505a3d0f73e578f55a
Ronald Munoz <ronald1195@icloud.com>
2018-06-12 22:13:03 -0600

    Ronald: Changed some dependencies. Added empty emailValidator class

---
+++ app/src/test/java/com/example/ronaldmunoz/myapplication/EmailValidator.java
@@ -0,0 +1,12 @@
+package com.example.ronaldmunoz.myapplication;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class EmailValidator {
+    /*@Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertThat(EmailValidator.isValidEmail("name@email.com"), is(true));
+    }*/
+}
