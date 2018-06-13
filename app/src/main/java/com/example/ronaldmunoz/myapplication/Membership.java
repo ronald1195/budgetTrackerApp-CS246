b9aa984738bea86b20803f31f1f10ae75c60da4e
Ronald Munoz <ronald1195@icloud.com>
2018-06-12 22:51:06 -0600

    Ronald: I created a Membership object with some functions I think that would be usefull. You can keep adding functions if you need something more specific to anything you guys are doing

---
+++ app/src/main/java/com/example/ronaldmunoz/myapplication/Membership.java
@@ -0,0 +1,74 @@
+package com.example.ronaldmunoz.myapplication;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Membership {
+    String userName;
+    String userEmail;
+    Date paymentDueDate;
+    String comments;
+
+    ///////////////////////////////////////////////
+    // Default constructor for Membership class
+    ///////////////////////////////////////////////
+    public Membership(){
+        userName = null;
+        userEmail = null;
+        paymentDueDate = null;
+        comments = null;
+    }
+
+    ///////////////////////////////////////////////
+    // Non-default constructor for Membership class
+    ///////////////////////////////////////////////
+    public Membership(String name, String email, Date dueDate, String comment){
+        userName = name;
+        userEmail = email;
+        paymentDueDate = dueDate;
+        comments = comment;
+    }
+
+    ///////////////////////////////////////////////
+    //  Some Getters
+    ///////////////////////////////////////////////
+    public void getMembershipInfo(){
+        System.out.print("Name on membership:  " + userName
+                        +"\nEmail:             " + userEmail
+                        +"\nPayment is due the " + dateToString(paymentDueDate) + " of every month"
+                        +"\nComments:          " + displayComments());
+    }
+
+    ///////////////////////////////////////////////
+    //  Function just to convert a date to a string.ß
+    ///////////////////////////////////////////////
+    public String dateToString(Date indate)
+    {
+        String dateString = null;
+        SimpleDateFormat sdfr = new SimpleDateFormat("dd");
+        /*you can also use DateFormat reference instead of SimpleDateFormat
+         * like this: DateFormat df = new SimpleDateFormat("dd/MMM/yyyy");
+         */
+        try{
+            dateString = sdfr.format( indate );
+        }catch (Exception ex ){
+            System.out.println(ex);
+        }
+        return dateString;
+    }
+
+    ///////////////////////////////////////////////
+    //  Function useful to handle empty comments.
+    ///////////////////////////////////////////////
+    public String displayComments(){
+        String emptyCommentMessage = "There is no comments for this membership";
+
+        if(comments != null){
+            return comments;
+        }
+        else{
+            return emptyCommentMessage;
+        }
+    }
+
+}
