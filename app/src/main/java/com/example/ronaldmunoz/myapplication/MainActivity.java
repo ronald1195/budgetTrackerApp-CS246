c6d58aaa96502e9d624830324a5cc0d521eadcde
EthanRawlins <39172729+EthanRawlins@users.noreply.github.com>
2018-06-13 11:53:35 -0600

    ListView

--- app/src/main/java/com/example/ronaldmunoz/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ronaldmunoz/myapplication/MainActivity.java
@@ -1,25 +1,37 @@
 package com.example.ronaldmunoz.myapplication;
 
+import android.app.ActionBar;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*String [] myStringArray = new String [15];
+        for (int i = 0; i < 15; i++)
+        {
+            myStringArray[i] = Integer.toString(i + 1);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1, myStringArray);
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);*/
+
         setContentView(R.layout.activity_main);
         //Ethan
     }
 
-    public int i = 0;
-    public int counter(int i) {
-        for (;i < 20; i++) {
-            return i;
-        }
-        return i;
-    }
+
+
+
 
     //Comment made by Ronald
 }
