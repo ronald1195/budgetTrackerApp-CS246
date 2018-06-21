d8d40d161173df555a8d90f102610225cbbb0e75
EthanRawlins <39172729+EthanRawlins@users.noreply.github.com>
2018-06-20 19:40:05 -0600

    ListView

--- app/src/main/java/com/example/ronaldmunoz/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ronaldmunoz/myapplication/MainActivity.java
@@ -5,19 +5,24 @@ import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        HomeDisplay homeDisplay = new HomeDisplay();
+        String [] list = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getListView().getContext(), android.R.layout.simple_expandable_list_item_1, list);
+        getListView().setAdapter(adapter);
         //homeDisplay.onCreate(savedInstanceState);
 
         //ListView mainListView = findViewById(R.id.mainListView);
@@ -26,8 +31,22 @@ public class MainActivity extends AppCompatActivity {
         /*ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1, myStringArray);
         ListView listView = findViewById(R.id.listView);
         listView.setAdapter(adapter);*/
+    }
 
-        setContentView(R.layout.activity_main);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /*String [] myStringArray = new String [15];
