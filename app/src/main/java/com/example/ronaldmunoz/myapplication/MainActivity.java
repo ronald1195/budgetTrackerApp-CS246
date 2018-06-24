29bbd9fff34cfe251cdafc02ef9ded51d912799a
Durid Ahmad <ahmad.durid96@gmail.com>
2018-06-23 22:08:20 -0600

    Main Activity layout modified

--- app/src/main/java/com/example/ronaldmunoz/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ronaldmunoz/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ronaldmunoz.myapplication;
 import android.app.ActionBar;
 import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -16,22 +17,34 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends ListActivity {
+
+    ArrayList<String> arrayList;
+    public static String jsonItem;
+    public static final String EDIT_ITEM = "com.example.ronaldmunoz.myapplication.ITEM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //I made some changes to be able to open the edit activity from selecting a listview item
-        String [] list = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getListView().getContext(), android.R.layout.simple_expandable_list_item_1, list);
+        final String [] list = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        arrayList = new ArrayList<>(Arrays.asList(list));
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getListView().getContext(), android.R.layout.simple_expandable_list_item_1, arrayList);
         getListView().setAdapter(adapter);
 
-        //When the user ong clicks on the list item he will get a dialog box
+        //When the user presses and holds on an item in the list he will get a dialog box
+        //containing an edit and a delete button, currently the delete one
+        //does nothing for the time being
         getListView().setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                showInputBox(arrayList.get(position),position);
                 return true;
             }
         });
@@ -70,11 +83,15 @@ public class MainActivity extends ListActivity {
         TextView txtMessage = dialog.findViewById(R.id.txtmessage);
         txtMessage.setText("Edit options");
         Button btEdit = dialog.findViewById(R.id.btedit);
+        //For temporary testing purposes
+        jsonItem = oldItem;
         dialog.show();
     }
 
     public void editActivity(View view) {
-        
+        Intent intent = new Intent(this, Edit.class);
+        intent.putExtra(EDIT_ITEM, jsonItem);
+        startActivity(intent);
     }
 
 
